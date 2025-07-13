bfdce59b86c450af8b7ff084b68598c6f047c77e
bapuqln <bapu_wang7@163.com>
2025-07-13 17:47:00 +0800

    try to fix pipeline issue

--- languages/objc/outline.scm
+++ languages/objc/outline.scm
@@ -1,28 +1,24 @@
 ;; Outline rules for Objective-C
 
-(class_interface
- name: (identifier) @name) @class
+((class_interface
+  (identifier) @name)) @class
 
-(class_implementation
- name: (identifier) @name) @class
+((class_implementation
+  (identifier) @name)) @class
 
-(protocol_declaration
- name: (identifier) @name) @protocol
+((protocol_declaration
+  (identifier) @name)) @protocol
 
-(category_interface
- name: (identifier) @class_name
- category: (identifier) @category_name) @category
+((class_interface
+  (identifier) @class_name
+  category: (identifier) @category_name)) @category
 
-(method_definition
- return_type: (_)? @type
- selector: (identifier) @name) @method
+((method_definition
+  (method_type)? @type
+  (identifier) @name)) @method
 
-(method_definition
- return_type: (_)? @type
- selector: (keyword_selector) @name) @method
-
-(function_definition
- declarator: (function_declarator
-               declarator: (identifier) @name)) @function
+((function_definition
+  declarator: (function_declarator
+    declarator: (identifier) @name))) @function
 
 ;; Add more specific rules as needed based on the grammar
--- languages/objc/tags.scm
+++ languages/objc/tags.scm
@@ -1,40 +1,34 @@
-tags.scm
+;; tags.scm
 
 ;; Class interfaces
-(class_interface
-  name: (identifier) @name) @definition.class
+((class_interface
+  (identifier) @name)) @definition.class
 
 ;; Class implementations
-(class_implementation
-  name: (identifier) @name) @definition.class
+((class_implementation
+  (identifier) @name)) @definition.class
 
 ;; Protocol declarations
-(protocol_declaration
-  name: (identifier) @name) @definition.interface
+((protocol_declaration
+  (identifier) @name)) @definition.interface
 
 ;; Method definitions
-(method_definition
-  selector: (identifier) @name) @definition.method
+((method_definition
+  (identifier) @name)) @definition.method
 
-(method_definition
-  selector: (keyword_selector) @name) @definition.method
-
-;; Property declarations
-(property_declaration
-  name: (identifier) @name) @definition.property
+;; Property declarations - commented out since it's causing errors
+;; ((property_declaration
+;;   (identifier) @name)) @definition.property
 
 ;; Function definitions
-(function_definition
+((function_definition
   declarator: (function_declarator
-    declarator: (identifier) @name)) @definition.function
+    declarator: (identifier) @name))) @definition.function
 
 ;; Method calls
-(message_expression
-  selector: (identifier) @name) @reference.call
-
-(message_expression
-  selector: (keyword_selector) @name) @reference.call
+((message_expression
+  (identifier) @name)) @reference.call
 
 ;; Function calls
-(call_expression
-  function: (identifier) @name) @reference.call
+((call_expression
+  function: (identifier) @name)) @reference.call
